0cfed3556f575399529661a585cf9e2d20cfaf21
AKD\etominac <EugenLuka.Tominac@akd.hr>
2024-01-04 15:06:40 +0100

    third

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	dog "github.com/Sporejug2/Dog"
 )
 
@@ -18,4 +20,16 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrowUp(Barks())
+}
+
+func From11() {
+	fmt.Println("im from version 1.1.0")
+}
+
+func From12() {
+	fmt.Println("Im from version 1.2.0")
+}
+
+func From13() {
+	fmt.Println("Im from version 1.3.0")
 }
